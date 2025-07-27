ccdd55a3d68ceae01775cf63b7c0acdf0a95de1f
DanielCaldas2711 <caldasdaniel12@gmail.com>
2025-07-28 00:59:53 +0100

    Added ErrorController and redirected error pages

---
+++ SistemaGestaoEscola.Web/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class ErrorController : Controller
+{
+    [Route("Error/{statusCode}")]
+    public IActionResult HttpStatusCodeHandler(int statusCode)
+    {
+        return statusCode switch
+        {
+            400 => View("ErrorViews/BadRequest"),
+            401 => View("ErrorViews/Unauthorized"),
+            403 => View("ErrorViews/Forbidden"),
+            404 => View("ErrorViews/NotFound"),
+            _ => View("ErrorViews/ServerError")
+        };
+    }
+
+    [Route("Error/500")]
+    public IActionResult InternalServerError()
+    {
+        return View("ErrorViews/ServerError");
+    }
+
+}
--- SistemaGestaoEscola.Web/Program.cs
+++ SistemaGestaoEscola.Web/Program.cs
@@ -114,10 +114,12 @@ namespace SistemaGestaoEscola.Web
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/Error/500");
                 app.UseHsts();
             }
+
+            //app.UseExceptionHandler("/Error/500");
+            app.UseStatusCodePagesWithReExecute("/Error/{0}");
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
@@ -126,8 +128,6 @@ namespace SistemaGestaoEscola.Web
 
             app.UseAuthentication();
             app.UseAuthorization();
-
-            //app.UseSession();
 
             app.MapControllerRoute(
                 name: "default",
